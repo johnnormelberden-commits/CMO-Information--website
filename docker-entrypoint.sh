7b5e397c50c711ea6e01350a3dce5a08db7b7674
johnnormelberden-commits <johnnormelberden@gmail.com>
2026-08-22 13:24:08 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 a2dismod mpm_event mpm_worker mpm_prefork || true
 a2enmod mpm_prefork
 
+sed -i "s/Listen 80/Listen ${PORT:-80}/" /etc/apache2/ports.conf
+sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT:-80}>/" /etc/apache2/sites-available/000-default.conf
+
 exec apache2-foreground
